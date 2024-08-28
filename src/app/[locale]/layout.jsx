3187046ffcfdceb59007c30fa381c35db25a4001
Bilal Haider <haiderbilal306@gmail.com>
2024-08-28 16:47:01 +0500

    common layout for all pages

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -4,6 +4,7 @@ import Providers from "@/lib/providers";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import PrelineScript from "../components/PrelineScript";
+import LayoutWrapper from "../components/LayoutWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default async function RootLayout({ children, params }) {
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages} >
           <Providers>
+            <LayoutWrapper>
             {children}
+            </LayoutWrapper>
           </Providers>
         </NextIntlClientProvider>
       </body>
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='border w-[1150px] flex h-[52px] py-3 pr-10 pl-5'>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
 
