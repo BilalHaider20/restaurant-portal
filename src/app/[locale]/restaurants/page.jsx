4537efca778e57e8605b9b671b9964d43aa30729
taahahussainkhan <taaha128@gmail.com>
2024-08-29 15:58:08 +0500

    Dashboard redirection to home page

--- src/app/[locale]/restaurants/page.jsx
+++ src/app/[locale]/restaurants/page.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { useTranslations } from "next-intl";
 import RestaurantCard from "../../components/RestaurantCard";
 import AddBtn from "../../components/buttons/AddBtn";
 import SearchInput from "@/app/components/SearchInput";
 import FilterDropDown from "../../components/buttons/FilterDropDown";
+import RestaurantModal from "@/app/components/RestaurantModal";
+
 
 const Page = () => {
-  const t = useTranslations('restaurants')
+  const t = useTranslations('restaurants');
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleAddClick = () => {
+    setModalOpen(true); 
+  };
+
+  const closeModal = () => {
+    setModalOpen(false); 
+  };
+
   return (
     <div className="bg-bg-highlight w-full max-w-[1150px] mx-auto p-4">
       <div className="flex flex-col sm:flex-row justify-between items-center">
@@ -14,7 +27,7 @@ const Page = () => {
         <div className="flex flex-col sm:flex-row items-center gap-2">
           <SearchInput width="100%" smWidth="379px" height="44px" />
           <FilterDropDown />
-          <AddBtn />
+          <AddBtn onClick={handleAddClick} /> 
         </div>
       </div>
       <div className="flex flex-col gap-3 mt-4">
@@ -23,6 +36,7 @@ const Page = () => {
         <RestaurantCard />
         <RestaurantCard />
       </div>
+      {isModalOpen && <RestaurantModal onClose={closeModal} />} 
     </div>
   );
 };
