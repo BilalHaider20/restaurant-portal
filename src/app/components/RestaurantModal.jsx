936a11b00444d4da691f8c4414ebec0dc3d312eb
Muhammad Shaheryar <shaheryarashraf824@gmail.com>
2024-08-30 16:06:20 +0500

    minor changes

--- src/app/components/RestaurantModal.jsx
+++ src/app/components/RestaurantModal.jsx
@@ -1,78 +1,143 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect,useRef} from 'react';
 import Image from 'next/image';
 import Icons from '../../../public/images/kit.png';
 import vector from '../../../public/images/vector.png';
+import center from '../../../public/images/center.png';
 import profile from '../../../public/images/profile.png';
 import camera from '../../../public/images/camera.png';
 import { useTranslations } from 'next-intl';
 
-const RestaurantModal = () => {
-  const [isModalClose, setIsModalClose] = useState(false);
-  const t = useTranslations(); // Assuming translations hook is correctly set up
+const RestaurantModal = ({ onClose }) => {
+  const [userInfo, setUserInfo] = useState({
+    restaurantName: '',
+    cuisine: '',
+    pricing: '',
+    restaurantDescription: ''
+  });
+  const [fileUpload, setFileUpload] = useState(null);
+  const [fileName, setFileName] = useState('');
+  const fileInputRef = useRef(null);
 
-  const handleModal = () => {
-    setIsModalClose(true);
+  useEffect(() => {
+    const savedInfo = localStorage.getItem('restaurantInfo');
+    if (savedInfo) {
+      setUserInfo(JSON.parse(savedInfo));
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo(prevInfo => ({
+      ...prevInfo,
+      [name]: value
+    }));
   };
 
-  const handleFileChange = (e) => {
-    // Handle file upload logic here
-    const file = e.target.files[0];
+  const handlePricingChange = (price) => {
+    setUserInfo(prevInfo => ({
+      ...prevInfo,
+      pricing: price
+    }));
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem('restaurantInfo', JSON.stringify(userInfo));
+    console.log(userInfo);
+    alert('Information saved successfully!');
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
     if (file) {
-      console.log("Selected file:", file.name);
+      setFileUpload(file);
+      setFileName(file.name);
+    } else {
+      setFileUpload(null);
+      setFileName('');
     }
   };
 
-  return (
-    <div className={`w-full max-w-3xl bg-[#F2F4F7] min-h-screen absolute mx-auto transition-all ease-linear transform
-      ${isModalClose ? 'right-[-800px]' : 'right-0'}`}>
-      <div className="font-semibold text-[#15223C] bg-[#E6E6E6] w-full p-4 flex justify-between items-center">
-        <h2 className="text-xl sm:text-2xl">Add a New Restaurant</h2>
-        <button className="text-xl" onClick={handleModal}>&times;</button>
-      </div>
+  const handleBrowseClick = () => {
+    fileInputRef.current.click();
+  };
 
+  const t = useTranslations('RestaurantModal');
+
+  return (
+    <div className="w-full h-[1123px] max-w-3xl bg-[#F2F4F7] min-h-screen absolute right-0 mx-auto transition-all ease-linear transform">
+      <div className="font-semibold text-[#15223C] bg-[#E6E6E6] w-full p-4 flex justify-between items-center">
+        <h2 className="text-xl sm:text-2xl">{t('newRestaurant')}</h2>
+        <button className="text-xl" onClick={onClose}>&times;</button>
+      </div>
+    
       <div className="bg-white rounded-lg m-4 p-4 space-y-6">
-        <div className="flex flex-col sm:flex-row items-center gap-4">
-          <div className="relative">
-            <Image src={profile} alt="Restaurant Profile" width={148} height={153} className="rounded-full" />
-            <button className="absolute bottom-0 right-0 bg-primary-blue text-white rounded-full px-3 py-1 flex items-center text-sm">
+      <div className="flex flex-col sm:flex-row items-center gap-4">
+          <div className="relative mb-2">
+            <Image src={profile} alt="Restaurant Profile" width={148} height={153} className="rounded-full " />
+            <button className="ml-[52px] transform -translate-x-1/2 bg-primary-blue text-white rounded-full px-3 py-1 flex items-center text-sm">
+
               <Image src={camera} alt="Camera" width={20} height={20} className="mr-1" />
               Add
             </button>
+            
           </div>
           <div className="flex-grow w-full">
-            <label className="block text-[#3C3C3C] font-semibold mb-2">Restaurant Name</label>
-            <input type="text" className="border border-gray-300 w-full p-2 rounded" placeholder="Enter Restaurant Name" />
+            <label className="block text-[#3C3C3C] font-semibold mb-2">{t('restaurantName')}</label>
+            <input
+              type="text"
+              name="restaurantName"
+              value={userInfo.restaurantName}
+              onChange={handleChange}
+              className="border border-gray-300 w-full p-2 rounded"
+              placeholder="Enter Restaurant Name"
+            />
           </div>
         </div>
 
-        <div className="flex flex-col sm:flex-row gap-4">
-          <div className="w-full sm:w-1/2">
-            <label className="block text-[#3C3C3C] font-semibold mb-2">Choose Cuisine</label>
+        <div className="flex flex-col ml-36 sm:flex-row gap-4">
+          <div className="w-[299px] sm:w-1/2">
+            <label className="block text-[#3C3C3C] font-semibold mb-2">{t('cusine')}</label>
             <div className="flex items-center border border-gray-300 rounded overflow-hidden">
               <div className="p-2">
                 <Image src={Icons} alt="Cuisine icon" width={24} height={24} />
               </div>
-              <input type="text" className="flex-grow p-2" placeholder="Select" />
+              <input
+                type="text"
+                name="cuisine"
+                value={userInfo.cuisine}
+                onChange={handleChange}
+                className="flex-grow p-2"
+                placeholder="Select"
+              />
               <div className="p-2">
                 <Image src={vector} alt="Vector icon" width={24} height={24} />
               </div>
             </div>
           </div>
 
-          <div className="w-full sm:w-1/2">
-            <label className="block text-[#3C3C3C] font-semibold mb-2">Pricing Category</label>
+          <div className="w-[191px] sm:w-1/2">
+            <label className="block text-[#3C3C3C] font-semibold mb-2">{t('pricing')}</label>
             <div className="flex items-center space-x-6">
-              {['$', '$$', '$$$'].map((price, index) => (
-                <button key={index} className="text-lg cursor-pointer hover:text-primary-blue">{price}</button>
+              {['$', '$$', '$$$'].map((price) => (
+                <button
+                  key={price}
+                  className={`text-lg cursor-pointer ${userInfo.pricing === price ? 'text-primary-blue' : 'hover:text-primary-blue'}`}
+                  onClick={() => handlePricingChange(price)}
+                >
+                  {price}
+                </button>
               ))}
             </div>
           </div>
         </div>
 
-        <div className="pt-1">
-          <label className="block text-[#3C3C3C] font-semibold mb-2">Restaurant Description</label>
-          <textarea 
+        <div className='pt-1'>
+          <label className="block text-[#3C3C3C] font-semibold mb-2">{t('restaurantDescription')}</label>
+          <textarea
+            name="restaurantDescription"
+            value={userInfo.restaurantDescription}
+            onChange={handleChange}
             className="w-full bg-white border border-gray-300 rounded-lg p-4 h-32 resize-none"
             placeholder="Enter Short Description"
           ></textarea>
@@ -81,28 +146,34 @@ const RestaurantModal = () => {
         <div>
           <label className="block text-[#3C3C3C] font-semibold mb-2">{t('promotionBanner')}</label>
           <div className="border-2 border-line rounded-lg p-8 text-center">
+            <Image src={center} alt="Upload icon" width={70} height={44} className="mx-auto mb-4" />
             <p className="text-[#1F2937]">
-              <label htmlFor="file-upload" className="text-blue-700 cursor-pointer">
-                {t('browse')}
-              </label>
-              <input
-                id="file-upload"
+              {t('dragFiles')} 
+              <button className="text-blue-700" onClick={handleBrowseClick}>{t('browse')}</button>
+              <input 
                 type="file"
-                className="hidden"
+                ref={fileInputRef}
                 onChange={handleFileChange}
+                className="hidden"
               />
             </p>
+            {fileName && (
+              <p className="mt-2 text-sm text-gray-500">
+                Selected file: {fileName}
+              </p>
+            )}
+            <p className="text-[#9CA3AF] text-sm mt-2">{t('maximumSize')}</p>
           </div>
         </div>
+      </div>
 
-        <div className="absolute bottom-0 left-0 right-0 bg-white p-4 flex justify-end space-x-4">
-          <button className="px-4 py-2 text-gray-600 bg-white border border-gray-300 rounded-lg hover:bg-gray-100" onClick={handleModal}>
-            Cancel
-          </button>
-          <button className="px-4 py-2 text-white bg-primary-blue rounded-lg hover:bg-blue-600">
-            Add
-          </button>
-        </div>
+      <div className="absolute bottom-0 left-0 right-0 bg-white p-4 flex justify-end space-x-4">
+        <button className="px-4 py-2 text-gray-600 bg-white border border-gray-300 rounded-lg hover:bg-gray-100" onClick={onClose}>
+          {t('cancel')}
+        </button>
+        <button className="px-4 py-2 text-white bg-primary-blue rounded-lg hover:bg-blue-600" onClick={handleSubmit}>
+          {t('add')}
+        </button>
       </div>
     </div>
   );
