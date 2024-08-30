255d2295baf1e64aa66916103c759c0561bf5253
hibah321 <hbhshahbaz321@gmail.com>
2024-08-30 14:13:37 +0500

    fixes

--- src/app/components/LayoutWrapper.jsx
+++ src/app/components/LayoutWrapper.jsx
@@ -32,7 +32,7 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex w-full mt-16">
         <Sidebar sidebarOpen={sidebarOpen} />
         <main className={`h-[calc(100vh-4rem)] overflow-y-scroll relative w-full flex-1 transition-all ease-in-out duration-300
-         ${sidebarOpen ? 'md:ml-[289px]' : 'md:ml-20'} 
+         } 
           `}>
           <div className="flex-1">{children}
           <Footer />
