4ca7e7c89ac121f60330869882ca36e93f4b5323
hibah321 <hbhshahbaz321@gmail.com>
2024-09-02 16:18:29 +0500

    fixed ImageUpload

--- .history/src/app/components/common/FormElements/ImageUpload_20240902161149.jsx
+++ .history/src/app/components/common/FormElements/ImageUpload_20240902161149.jsx
@@ -3,24 +3,20 @@ import { useDropzone } from 'react-dropzone';
 import Image from 'next/image';
 import center from '../../../../../public/images/center.png';
 
-const FileUpload = ({ title }) => {
-  const [previews, setPreviews] = useState([]);
+const FileUpload = ({title}) => {
+  const [preview, setPreview] = useState(null);
 
   const onDrop = useCallback((acceptedFiles) => {
-    const newPreviews = acceptedFiles.map((file) => {
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
       const reader = new FileReader();
-      reader.readAsDataURL(file);
-      
-      return new Promise((resolve) => {
-        reader.onloadend = () => {
-          resolve(reader.result);
-        };
-      });
-    });
 
-    Promise.all(newPreviews).then((previewList) => {
-      setPreviews((prev) => [...prev, ...previewList]);
-    });
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -34,23 +30,21 @@ const FileUpload = ({ title }) => {
       >
         <input {...getInputProps()} />
         {isDragActive ? (
-          <p className="text-[#1F2937]">Drop the files here...</p>
-        ) : (
           <>
-            <Image src={center} alt="Upload icon" width={70} height={44} className="mx-auto mb-4" />
-            <p className="text-[#1F2937]">
-              Drop your files here or <span className="text-blue-700 cursor-pointer">browse</span>
-            </p>
+          <p className="text-[#1F2937]">Drop the files here...</p>
+          </>
+        ) : (
+          <> 
+          <Image src={center} alt="Upload icon" width={70} height={44} className="mx-auto mb-4" />
+          <p className="text-[#1F2937]">
+            Drop your files here or <span className="text-blue-700 cursor-pointer">browse</span>
+          </p>
           </>
         )}
         <p className="text-[#9CA3AF] text-sm mt-2">Maximum size: 50MB</p>
       </div>
-      <div className="flex flex-wrap gap-4 mt-4">
-        {previews.map((preview, index) => (
-          <div key={index} className="relative">
-            <Image src={preview} alt={`Preview ${index + 1}`} width={100} height={100} className="rounded-md" />
-          </div>
-        ))}
+      <div>
+        
       </div>
     </div>
   );
